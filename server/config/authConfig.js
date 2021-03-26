6698850321c79b5c92078fa0ba7cf3bea7a7bf1c
Wei Sheng <weisheng_aw@hotmail.com>
2021-03-26 00:53:21 -0700

    Login/Logout/Register Works

---
+++ server/config/authConfig.js
@@ -0,0 +1,4 @@
+module.exports ={
+    secret: "in-west-philadelphia-born-and-raised",
+    mongo: "mongodb+srv://weishengaw2:Aventad0r@cluster0.llzce.mongodb.net/myFirstDatabase?retryWrites=true&w=majority" 
+};
