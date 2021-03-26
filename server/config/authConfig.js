367e9a0acbd44a4ad9fa2eab736d0a00d7ee850b
Wei Sheng <weisheng_aw@hotmail.com>
2021-03-26 01:11:54 -0700

    Added .gitignore

--- server/config/authConfig.js
+++
@@ -1,4 +0,0 @@
-module.exports ={
-    secret: "in-west-philadelphia-born-and-raised",
-    mongo: "mongodb+srv://weishengaw2:Aventad0r@cluster0.llzce.mongodb.net/myFirstDatabase?retryWrites=true&w=majority" 
-};
