6277ad0b299a3d304fd5ded85537a547d9d2a0b7
Wei Sheng <weisheng_aw@hotmail.com>
2021-03-29 14:53:14 -0700

    changed index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 
 // setup cors options
 var corsOptions = {
-    origin: "https://weishengaw.github.io/InternShips"
+    origin: "https://weishengaw.github.io"
 }
 app.use(cors(corsOptions))
 app.use(express.json());
